a0885835fbab81b581bb622c4cbd1469f893a10c
Terry Hunt <thunt4jr@gmail.com>
2022-09-15 18:19:04 -0400

    update

--- pages/resume.js
+++ pages/resume.js
@@ -115,10 +115,70 @@ const experience = [
     ],
   },
 ];
-
-export default function resume() {
+const DOMAIN = "https://terryhunt.dev";
+const DEFAULT_OG_IMAGE = "";
+export default function resume({
+  title = "Terry Hunt's Resume for Full-Stack Developer Position",
+  description = "Resume of Terry Hunt for a Full-Stack Developer position along with WordPress, Prestashop, Email HTML, Newsletter, and ReactJS experience. ",
+  siteName = "Resume Page",
+  canonical = DOMAIN,
+  ogImage = DEFAULT_OG_IMAGE,
+  ogType = "website",
+  twitterHandle = "@TerryHuntDev",
+}) {
   return (
     <>
+      <Head>
+        <title key='title'>{`${title} – ${siteName}`}</title>
+        <meta name='description' content={description} />
+        <meta key='og_type' property='og:type' content={ogType} />
+        <meta key='og_title' property='og:title' content={title} />
+        <meta
+          key='og_description'
+          property='og:description'
+          content={description}
+        />
+        <meta key='og_locale' property='og:locale' content='en_IE' />
+        <meta key='og_site_name' property='og:site_name' content={siteName} />
+        <meta key='og_url' property='og:url' content={canonical ?? DOMAIN} />
+        <meta key='og_site_name' property='og:site_name' content={siteName} />
+        <meta
+          key='og_image'
+          property='og:image'
+          content={ogImage ?? DEFAULT_OG_IMAGE}
+        />
+        <meta
+          key='og_image:alt'
+          property='og:image:alt'
+          content={`${title} | ${siteName}`}
+        />
+        <meta key='og_image:width' property='og:image:width' content='1200' />
+        <meta key='og_image:height' property='og:image:height' content='630' />
+
+        <meta name='robots' content='index,follow' />
+
+        <meta
+          key='twitter:card'
+          name='twitter:card'
+          content='summary_large_image'
+        />
+        <meta key='twitter:site' name='twitter:site' content={twitterHandle} />
+        <meta
+          key='twitter:creator'
+          name='twitter:creator'
+          content={twitterHandle}
+        />
+        <meta key='twitter:title' property='twitter:title' content={title} />
+        <meta
+          key='twitter:description'
+          property='twitter:description'
+          content={description}
+        />
+
+        <link rel='canonical' href={canonical ?? DOMAIN} />
+
+        <link rel='shortcut icon' href='/favicon.ico' />
+      </Head>
       <Navbar />
       <div className='container'>
         <div className='row px-3'>
@@ -136,7 +196,8 @@ export default function resume() {
               <div className='d-flex mb-2' key={index}>
                 <Link href={link.href} target={link.target} rel={link.rel}>
                   <a>
-                    <FontAwesomeIcon icon={link.icon} /> {link.word}
+                    <FontAwesomeIcon icon={link.icon} height={"15px"} />{" "}
+                    {link.word}
                   </a>
                 </Link>
               </div>
