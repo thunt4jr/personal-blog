b5750830043a785883ea6c73ac40f497a16e8086
Terry Hunt <terry.hunt@sofwerx.org>
2024-06-27 11:49:54 -0400

    Refactor contact form component for improved accessibility and code consistency

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -3,96 +3,96 @@ import React from "react";
 const Contact = () => {
   return (
     <div
-      className='container-fluid mt-5'
-      id='contact'
-      style={{ background: "rgb(165, 201, 202, 0.3)" }}
+      className="container-fluid mt-5"
+      id="contact"
+      style={{ background: "rgba(165, 201, 202, 0.3)" }}
     >
-      <div className='container py-3'>
-        <h4 className='text-center'>
-          <small className='lead'>Reach out to</small> Terry Hunt
+      <div className="container py-3">
+        <h4 className="text-center">
+          <small className="lead">Reach out to</small> Terry Hunt
         </h4>
         <hr />
         <p>
           Feel free to contact Terry Hunt anytime about jobs you have available
           or projects that you need to be done for your business.
         </p>
-        <form method='POST' name='contact' action='/' data-netlify='true'>
-          <input type='hidden' name='form-name' value='contact' />
-          <div className='row'>
-            <div className='col'>
-              <div className='form-floating mb-3'>
+        <form method="POST" name="contact" action="/" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
+          <div className="row">
+            <div className="col">
+              <div className="form-floating mb-3">
                 <input
-                  type={"email"}
-                  className='form-control'
-                  id='floatingInput'
-                  placeholder='name@example.com'
-                  name='email'
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  placeholder="name@example.com"
+                  name="email"
                   required
                 />
-                <label htmlFor='email'>Email Address</label>
+                <label htmlFor="email">Email Address</label>
               </div>
             </div>
-            <div className='col'>
-              <div className='form-floating mb-3'>
+            <div className="col">
+              <div className="form-floating mb-3">
                 <input
-                  type={"tel"}
-                  className='form-control'
-                  id='floatingInput'
-                  placeholder='(888)888-8888'
-                  name='phone'
+                  type="tel"
+                  className="form-control"
+                  id="phone"
+                  placeholder="(888)888-8888"
+                  name="phone"
                   required
                 />
-                <label htmlFor='phone'>Phone Number</label>
+                <label htmlFor="phone">Phone Number</label>
               </div>
             </div>
           </div>
-          <div className='row'>
-            <div className='col'>
-              <div className='form-floating mb-3'>
+          <div className="row">
+            <div className="col">
+              <div className="form-floating mb-3">
                 <input
-                  type={"text"}
-                  id='floatingInput'
-                  className='form-control'
-                  placeholder='First Name'
-                  name='first-name'
+                  type="text"
+                  id="first-name"
+                  className="form-control"
+                  placeholder="First Name"
+                  name="first-name"
                   required
                 />
-                <label htmlFor='first-name'>First Name</label>
+                <label htmlFor="first-name">First Name</label>
               </div>
             </div>
-            <div className='col'>
-              <div className='form-floating mb-3'>
+            <div className="col">
+              <div className="form-floating mb-3">
                 <input
-                  type={"text"}
-                  id='floatingInput'
-                  className='form-control'
-                  placeholder='Last Name'
-                  name='last-name'
+                  type="text"
+                  id="last-name"
+                  className="form-control"
+                  placeholder="Last Name"
+                  name="last-name"
                   required
                 />
-                <label htmlFor='last-name'>Last Name</label>
+                <label htmlFor="last-name">Last Name</label>
               </div>
             </div>
-            <div>
-              <div className='col'>
-                <div className='form-floating mb-3'>
-                  <textarea
-                    className='form-control mb-3'
-                    placeholder='Message for Terry Hunt'
-                    id='floatingTextarea'
-                    style={{ height: "100px" }}
-                    name='Message'
-                    required
-                  />
-                  <label htmlFor='floatingTextarea' className='text-dark'>
-                    Message
-                  </label>
-                  <div className='col text-center'>
-                    <button type='submit' className='btn'>
-                      Submit
-                    </button>
-                  </div>
-                </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="form-floating mb-3">
+                <textarea
+                  className="form-control mb-3"
+                  placeholder="Message for Terry Hunt"
+                  id="message"
+                  style={{ height: "100px" }}
+                  name="message"
+                  required
+                />
+                <label htmlFor="message" className="text-dark">
+                  Message
+                </label>
+              </div>
+              <div className="col text-center">
+                <button type="submit" className="btn btn-primary">
+                  Submit
+                </button>
               </div>
             </div>
           </div>
